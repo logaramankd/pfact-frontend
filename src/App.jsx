8acede82629f48ad7e0110d5a772bbc1c2fe3b0d
Logaraman <logaraman2403@gmail.com>
2025-07-13 12:42:48 +0530

    add filter and edit logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react"
 import AppRouter from "./routes/AppRouter"
+import { createTheme, CssBaseline, IconButton, ThemeProvider } from "@mui/material"
+import { Brightness4, Brightness7 } from "@mui/icons-material"
 
 function App() {
+  const [mode, setMode] = useState("light")
+
+  const theme = createTheme({
+    palette: {
+      mode: mode
+    },
+  })
+
+  const toggleMode = () => {
+    setMode((prev) => (prev == 'light' ? "dark" : "light"))
+  }
   return (
-    <AppRouter/>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppRouter mode={mode} toggleMode={toggleMode} />
+    </ThemeProvider>
   )
 }
 
